2b49daaec99a2918909f6508668c34ab1aec04aa
Grzegorz666dev <grzegorz666dev@gmail.com>
2024-01-28 17:21:59 +0100

    delete hint

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-// Zmienna globalna przechowująca aktualny stan kalkulatora
 let calculatorState = "";
 
-// Funkcja dodająca znak do wyświetlacza
 function appendCharacter(char) {
     calculatorState += char;
     updateDisplay();
 }
 
-// Funkcja czyszcząca wyświetlacz
 function clearDisplay() {
     calculatorState = "";
     updateDisplay();
 }
 
-// Funkcja obliczająca wynik i aktualizująca wyświetlacz
 function calculateResult() {
     try {
         const result = eval(calculatorState);
         calculatorState = result.toString();
         updateDisplay();
     } catch (error) {
-        // Obsługa błędów
         calculatorState = "Error";
         updateDisplay();
     }
 }
 
-// Funkcja aktualizująca wyświetlacz
 function updateDisplay() {
     document.getElementById("display").value = calculatorState;
 }
 
-// Dodanie obsługi zdarzeń dla przycisków
 document.getElementById("addBtn").addEventListener("click", function () {
     appendCharacter('+');
 });
